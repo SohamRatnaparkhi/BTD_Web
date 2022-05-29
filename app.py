d0f0f7acbdf526c0894fde5cf4cfb446c9207fbd
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2022-05-30 02:36:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+import os
+
+model1 = load_model("static\models\model_1.h5")
+model2 = load_model("static\models\model_2.h5")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+NAME = ""
+PATH = ""
+
+
+@app.route('/prediction1', methods=['POST'])
+def predictor1():
+    image = request.files['image']
+    name = request.form.get('name')
+    global NAME
+    NAME = name
+    print(f"Name = {name}")
+    path = fr'static\user_images\{name}.jpg'
+    global PATH
+    PATH = path
+    image.save(path)
+    image = Image.open(image.stream)
+    image = image.resize((224, 224))
+    image.save(path)
+    image = image.convert('RGB')
+    image = cv2.imread(path)
+    img = np.array(image) / 255.0
+    img = np.expand_dims(img, axis=0)
+    result = model1.predict(img)
+    result = result.squeeze()
+    if result > 0.5:
+        print("Tumor")
+        p = result.squeeze() * 100
+        return render_template('prediction1.html', data=[f"TUMOR DETECTED", name])
+    else:
+        p = 100 - (result.squeeze() * 100)
+        print("No tumor")
+        return render_template('prediction1.html', data=[f"NO TUMOR DETECTED", name])
+    # return render_template('index.html', data=name)
+
+
+@app.route('/prediction2', methods=['POST'])
+def predictor2():
+    image = request.files['image']
+    name = request.form.get('name')
+    global NAME
+    NAME = name
+    print(f"Name = {name}")
+    path = fr'static\user_images\{name}.jpg'
+    global PATH
+    PATH = path
+    image.save(path)
+    image = Image.open(image.stream)
+    image = image.resize((224, 224))
+    image.save(path)
+    image = image.convert('RGB')
+    image = cv2.imread(path)
+    img = np.array(image) / 255.0
+    img = np.expand_dims(img, axis=0)
+    result = model2.predict(img)
+    result = result.squeeze()
+    if result > 0.5:
+        print("Tumor")
+        p = result.squeeze() * 100
+        return render_template('prediction2.html', data=[f"Tumor detected", name])
+    else:
+        p = 100 - (result.squeeze() * 100)
+        print("No tumor")
+        return render_template('prediction2.html', data=[f"No Tumor detected", name])
+    # return render_template('ind
+
+
+@app.route('/load_img')
+def load_img():
+    # return render_template('index.html', data=PATH)
+    global PATH, NAME
+    print(PATH, NAME)
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], f'{NAME}.jpg')
+    print(full_filename)
+    render_template('prediction1.html', name=NAME)
+    # return redirect(url_for('static', path=PATH), code=301)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+    app.config['UPLOAD_FOLDER'] = PATH
+    # app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
